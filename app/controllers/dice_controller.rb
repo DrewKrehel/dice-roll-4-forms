77a2e10a716f887f9efd8b82d52051c88fcb82d0
Drew Krehel <drewkrehel@gmail.com>
2025-09-07 19:41:59 +0000

    Now its done for real

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -4,7 +4,7 @@ class DiceController < ApplicationController
   end
 
   def roll_form
-    @number = params[:number].to_i
+    @number = params[:dice].to_i
     @sides = params[:sides].to_i
     @rolls = Array.new(@number) { rand(1..@sides) }
   

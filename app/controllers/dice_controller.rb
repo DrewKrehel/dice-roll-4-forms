4e273a3ae179ea52bee7b970870e2c0a1e55f2b6
Drew Krehel <drewkrehel@gmail.com>
2025-09-07 18:05:37 +0000

    added index and roll erbs

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,1 +1,13 @@
-class DiceController < ApplicatoinCon
+class DiceController < ApplicationController
+  def index
+    render :index
+  end
+
+  def roll_form
+    @number = params[:number].to_i
+    @sides = params[:sides].to_i
+    @rolls = Array.new(@number) { rand(1..@sides) }
+  
+    render :roll
+  end
+end
